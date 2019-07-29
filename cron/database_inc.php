5d10f8b3c29257b50767153d59292f4ee82e0de2
Tung Nguyen <tungnd.aiti@gmail.com>
2019-07-29 13:17:41 +0700

    delete all

--- cron/database_inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db['md']['server'] = 'localhost';
-$db['md']['username'] = 'root';
-$db['md']['password'] = '1';
-$db['md']['database'] = 'md';
-
-return $db;
--- public/js/thapthanh.js
+++
@@ -1,8 +0,0 @@
-var thapthanh_captcha = {
-    getCaptcha: function(){
-        var ts = new Date().getTime();
-        $("#captcha_img").attr("src", "/captcha/generate?" + ts);  
-        $("#captcha_img").show();      
-        return false;    
-    },
-}
